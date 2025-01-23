fe96f8d363c49a8378155c841c9f9129cc3d3094
Graham Fletcher <graham.fletcher@bip-group.com>
2025-01-23 22:48:29 +0000

    v6

--- gem.js
+++ gem.js
@@ -131,7 +131,7 @@ async function gemCall(parameters) {
   
     } catch (error) {
       console.error('Error calling Cloud Function:', error);
-      throw error
+      throw error  
     }
   }
   
